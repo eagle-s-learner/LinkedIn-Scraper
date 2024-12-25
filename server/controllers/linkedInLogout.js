e33cd94236d066355a6c8c5fdbdf5c032aed070e
satyam kr <satyam.9242@gmail.com>
2024-12-25 18:23:31 +0530

    handle linkedin logout

---
+++ server/controllers/linkedInLogout.js
@@ -0,0 +1,56 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs").promises;
+
+const sleep = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+async function Logout(req, res) {
+    try {
+        // launching browser, creating page
+        const browser = await puppeteer.launch({
+            headless: false,
+            args: [
+                "--start-maximized", // open the window in full screen
+            ],
+        });
+        const page = await browser.newPage();
+
+        page.setDefaultNavigationTimeout(0);
+
+        await page.setViewport({
+            width: 1366,
+            height: 768,
+        }); // resize the browser web page
+
+        await sleep(2000);
+
+        // fetching the cookie from cookie.json and setting it to browser instance
+        const cookieString = await fs.readFile("./cookies.json");
+        const cookies = JSON.parse(cookieString);
+
+        await browser.setCookie(...cookies);
+
+        await page.goto("https://www.linkedin.com/feed/");
+
+        await sleep(1000 * 20);
+
+        // clicking the 6th item from nav list to get sign out option
+        const navItems = await page.$$(".global-nav__primary-items li");
+        await navItems[5].click();
+
+        await sleep(2000);
+
+        // Selecting the sign out option from dropdown
+        await page.locator('a[href="/m/logout/"]').click();
+        
+        await browser.close();
+
+        await fs.writeFile('./cookies.json', '')
+        res.status(200).send("Logout success!!");
+    } catch (error) {
+        console.log("Error", error.message );
+    }
+}
+
+module.exports = Logout;
--- server/router/linkedInLogout.js
+++ server/router/linkedInLogout.js
@@ -1,34 +1,14 @@
 const express = require("express");
-const puppeteer = require("puppeteer");
-const { browserWSEndpoint } = require("./linkedInLogin");
-// const { browser } = require("../controllers/puppeteer");
+const Logout = require("../controllers/linkedInLogout");
 
 const router = express.Router();
 
-const linkedin_pass = process.env.LINKEDIN_PASS;
-const email = process.env.EMAIL;
 
-router.post("/logout/", async (req, res) => {
-    const browser = await puppeteer.connect({
-        browserWSEndpoint: browserWSEndpoint,
-    });
-    const page = await browser.newPage();
-    page.setDefaultNavigationTimeout(0);
-    await page.setViewport({
-        width: 1366,
-        height: 768,
-    }); // resize the browser web page
-
-    const cookieString = await fs.readFile("./cookies.json");
-    const cookies = JSON.parse(cookieString);
-
-    await browser.setCookie(...cookies);
-
-    await page.goto("https://www.linkedin.com");
-
-    // here the code for logout
-
-    res.status(200).send("Logout success!!");
-});
+router.post("/logout/", Logout);
 
 module.exports = router;
+
+// await page
+//     .locator(
+//         'ul[class="global-nav__primary-items]'
+//     ).click();
